5d1bfbe800fdd531845a98976f3709d6b343717c
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-01-11 12:05:29 -0200

    Remove department

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -11,6 +11,7 @@ import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -83,7 +84,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 			preparedStatement.executeUpdate();
 
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		} finally {
 			DB.closeStatement(preparedStatement);
 		}
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -23,4 +23,8 @@ public class DepartmentService {
 		}
 	}
 	
+	public void remove(Department department) {
+		dao.deleteById(department.getId());
+	}
+	
 }
